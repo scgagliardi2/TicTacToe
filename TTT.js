55cae5bf9ccb256bb2f1b0b7a7a9b745efb196d0
scgagliardi2 <scgagliardi2@gmail.com>
2020-01-13 17:56:38 -0600

    fixed stuff

--- TTT.js
+++ TTT.js
@@ -1,5 +1,5 @@
-var Game = True;
 var turn = 'O';
+var GameWon = false;
         
 function changeTurn() { 
     if (turn == 'O') {
@@ -15,14 +15,17 @@ function changeTurn() {
 }
 
 function placeMove(id, letter) { 
-    if (document.getElementById(id).innerHTML == 'X' || document.getElementById(id).innerHTML == 'O'){
-        document.getElementById(tttInfo).innerHTML = "You cannot play there."
-        return;
-    } else {
-        document.getElementById(tttInfo).innerHTML = ""
-        document.getElementById(id).innerHTML = letter
-        changeTurn();
-        placeWin(letter);
+    if (!GameWon) {
+        if (document.getElementById(id).innerHTML == 'X' || document.getElementById(id).innerHTML == 'O'){
+            document.getElementById('tttInfo').innerHTML = "You cannot play there."
+            return;
+        } 
+        else {
+            document.getElementById('tttInfo').innerHTML = ""
+            document.getElementById(id).innerHTML = letter
+            changeTurn();
+            placeWin(letter);
+        }
     }
 }
 
@@ -30,57 +33,64 @@ function placeWin(letter) {
     if (document.getElementById('tl').innerHTML == document.getElementById('tm').innerHTML) {
         if (document.getElementById('tl').innerHTML == document.getElementById('tr').innerHTML) {
             if (document.getElementById('tl').innerHTML == 'X' || document.getElementById('tl').innerHTML == 'O') {
-            document.getElementById('tttDisplay').innerHTML = letter + " wins!"
+            document.getElementById('tttDisplay').innerHTML = letter + " wins!";
+            GameWon = true;
             }
         }
     }
     if (document.getElementById('ml').innerHTML == document.getElementById('mm').innerHTML) {
         if (document.getElementById('ml').innerHTML == document.getElementById('mr').innerHTML) {
             if (document.getElementById('ml').innerHTML == 'X' || document.getElementById('ml').innerHTML == 'O') {
-            document.getElementById('tttDisplay').innerHTML = letter + " wins!"
+            document.getElementById('tttDisplay').innerHTML = letter + " wins!";
+            GameWon = true;
             }
         }
     }
     if (document.getElementById('bl').innerHTML == document.getElementById('bm').innerHTML) {
         if (document.getElementById('bl').innerHTML == document.getElementById('br').innerHTML) {
             if (document.getElementById('bl').innerHTML == 'X' || document.getElementById('bl').innerHTML == 'O') {
-            document.getElementById('tttDisplay').innerHTML = letter + " wins!"
+            document.getElementById('tttDisplay').innerHTML = letter + " wins!";
+            GameWon = true;
             }
         }
     }
     if (document.getElementById('tl').innerHTML == document.getElementById('ml').innerHTML) {
         if (document.getElementById('tl').innerHTML == document.getElementById('bl').innerHTML) {
             if (document.getElementById('tl').innerHTML == 'X' || document.getElementById('tl').innerHTML == 'O') {
-            document.getElementById('tttDisplay').innerHTML = letter + " wins!"
+            document.getElementById('tttDisplay').innerHTML = letter + " wins!";
+            GameWon = true;
             }
         }
     }
     if (document.getElementById('tm').innerHTML == document.getElementById('mm').innerHTML) {
         if (document.getElementById('tm').innerHTML == document.getElementById('bm').innerHTML) {
             if (document.getElementById('tm').innerHTML == 'X' || document.getElementById('tm').innerHTML == 'O') {
-            document.getElementById('tttDisplay').innerHTML = letter + " wins!"
+            document.getElementById('tttDisplay').innerHTML = letter + " wins!";
+            GameWon = true;
             }
         }
     }
     if (document.getElementById('tr').innerHTML == document.getElementById('mr').innerHTML) {
         if (document.getElementById('tr').innerHTML == document.getElementById('br').innerHTML) {
             if (document.getElementById('tr').innerHTML == 'X' || document.getElementById('tr').innerHTML == 'O') {
-            document.getElementById('tttDisplay').innerHTML = letter + " wins!"
+            document.getElementById('tttDisplay').innerHTML = letter + " wins!";
+            GameWon = true;
             }
         }
     }
     if (document.getElementById('tl').innerHTML == document.getElementById('mm').innerHTML) {
         if (document.getElementById('tl').innerHTML == document.getElementById('br').innerHTML) {
             if (document.getElementById('tl').innerHTML == 'X' || document.getElementById('tl').innerHTML == 'O') {
-            document.getElementById('tttDisplay').innerHTML = letter + " wins!"
+            document.getElementById('tttDisplay').innerHTML = letter + " wins!";
+            GameWon = true;
             }
         }
     } else if (document.getElementById('tr').innerHTML == document.getElementById('mm').innerHTML) {
         if (document.getElementById('tr').innerHTML == document.getElementById('bl').innerHTML) {
             if (document.getElementById('tr').innerHTML == 'X' || document.getElementById('tr').innerHTML == 'O') {
-                document.getElementById('tttDisplay').innerHTML = letter + " wins!"
+                document.getElementById('tttDisplay').innerHTML = letter + " wins!";
+                GameWon = true;
             }
         }
     }
 }
-
